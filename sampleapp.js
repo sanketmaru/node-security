513d06e2c1a1519d4330e9ebe05437fa2fe54a9f
sanket.maru <sanket.maru@weboniselab.com>
2014-09-21 05:31:26 +0530

    Renaming sample files

--- sampleapp.js
+++ sampleapp.js
@@ -7,7 +7,7 @@ newMod = require('./lib');
 var app = express();
 
 app.use(express.cookieParser());
-app.use(express.session({secret: '1234567890QWERTY'}));
+app.use(express.session({secret: 'xyz'}));
 
 app.get('/login', newMod.signIn, function(req, res){
 	res.jsonp({
@@ -44,15 +44,6 @@ app.get('/user', newMod.checkUserRole, newMod.checkUrlPattern, function(req, res
 	})
 });
 
-/*app.get('/radical',newMod.checkUserRole, function(req, res) {
-    if(req.session.userId) {
-        res.send('Last page was: ' + req.session.lastPage + '. ');
-    } else {
-
-    }
-});*/
-//If you host the application on Modulus the PORT environment variable will be defined,
-//otherwise I’m simply using 8080.
 app.listen(process.env.PORT || 9001);
 
 
